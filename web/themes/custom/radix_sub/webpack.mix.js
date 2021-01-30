ff7d7f39b5ec7699998f8aef06cecb13d6dbd2dd
Paul <Paul Glazkow>
2021-01-30 09:54:12 +0000

    init

---
+++ web/themes/custom/radix_sub/webpack.mix.js
@@ -0,0 +1,58 @@
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your application. See https://github.com/JeffreyWay/laravel-mix.
+ |
+ */
+const proxy = 'http://freedom.ti-work.ru';
+const mix = require('laravel-mix');
+const { exec } = require("child_process");
+/*
+ |--------------------------------------------------------------------------
+ | Configuration
+ |--------------------------------------------------------------------------
+ */
+mix
+    .setPublicPath('assets')
+    .disableNotifications()
+    .options({
+        processCssUrls: false
+    });
+
+if (!mix.inProduction()) {
+    mix.webpackConfig({
+        devtool: 'inline-source-map'
+    }).sourceMaps()
+}
+/*
+ |--------------------------------------------------------------------------
+ | Browsersync
+ |--------------------------------------------------------------------------
+ */
+mix.browserSync({
+    proxy: proxy,
+    files: [ 
+        'assets/js/**/*.js',
+        'assets/css/**/*.css',
+        // 'templates/**/*.twig'
+    ],
+    stream: true,
+});
+
+
+/*
+ |--------------------------------------------------------------------------
+ | SASS
+ |--------------------------------------------------------------------------
+ */
+mix.sass('src/sass/radix_sub.style.scss', 'css');
+
+/*
+ |--------------------------------------------------------------------------
+ | JS
+ |--------------------------------------------------------------------------
+ */
+mix.js('src/js/radix_sub.script.js', 'js');
